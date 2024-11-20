dd1e73bd28144b65494517908634c9e58b2d9f02
Egxr41k <egor2005krava@gmail.com>
2024-11-20 14:27:48 +0200

    updating create-category

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -50,11 +50,11 @@ export class CategoryService {
 		return category
 	}
 
-	async create() {
+	async create(dto: CategoryDto) {
 		return this.prisma.category.create({
 			data: {
-				name: '',
-				slug: ''
+				name: dto.name,
+				slug: slug(dto.name)
 			}
 		})
 	}
